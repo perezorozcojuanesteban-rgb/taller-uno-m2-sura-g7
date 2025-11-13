3dd9c76c50e8710c7c56f56a26ffa7a649a07809
Juan <perezorozcojuanesteban@gmail.com>
2025-11-12 23:28:48 -0500

    Se da solucion al ejercicio dos del taller

--- script.js
+++ script.js
@@ -26,3 +26,37 @@ console.log(compras);
 compras.splice(1, 1);
 
 console.log(compras);
+
+
+
+/*   ### Ejercicio 2: Pila de Tareas Pendientes (LIFO)
+
+Inicias con la siguiente pila de tareas:
+`let tareas = ["Revisar emails"];`
+
+**Tareas:**
+1.  Llega una tarea nueva: "Llamar al cliente". Ponla **ENCIMA** de la pila (al inicio).
+2.  Llega otra tarea: "Preparar reporte". Ponla **ENCIMA** de la pila (al inicio).
+3.  Completaste la última tarea que entró ("Preparar reporte"). Retírala de la pila.
+4.  Completaste la siguiente tarea ("Llamar al cliente"). Retírala.
+    */
+
+let tareas = ["Revisar emails"];
+
+// Nueva tarea "Llamar cliente" al inicio de la pila
+
+tareas.unshift("Llamar al cliente");
+
+// Nueva tarea "Preparar reporte" al inicio de la pila
+
+tareas.unshift("Preparar reporte");
+
+// Retirar tarea compleatada "Preparar reporte"
+
+tareas.shift();
+
+// Retirar tarea compleatada "Llamar cliente"
+
+tareas.shift();
+
+console.log(tareas);
